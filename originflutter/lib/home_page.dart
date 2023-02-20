676fc8612a8a2b832e6838a785a29633debbc09a
TamTM <minhtamldt@gmail.com>
2023-02-20 23:51:01 +0700

    save code demo textfield

--- originflutter/lib/home_page.dart
+++ originflutter/lib/home_page.dart
@@ -9,12 +9,116 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String username = "";
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.white,
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Container(
+                child: Text('CACUALTE', style: TextStyle(
+                    fontSize: 30, fontWeight: FontWeight.bold)),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(width: 5, color: Colors.red)
+                    ),
+                    hintText: 'Input Number A'
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(height: 10),
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(width: 5, color: Colors.red)
+                    ),
+                    hintText: 'Input Number B'
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              Container(
+                  height: 50,
+                  child: Center(child: Text('Result: '))),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          foregroundColor: Colors.white,
+                          padding: EdgeInsets.all(16.0),
+                          textStyle: TextStyle(fontSize: 20)
+                      ),
+                      onPressed: () {},
+                      child: Text('+')),
+                  SizedBox(width: 10),
+                  TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          foregroundColor: Colors.white,
+                          padding: EdgeInsets.all(16.0),
+                          textStyle: TextStyle(fontSize: 20)
+                      ),
+                      onPressed: () {},
+                      child: Text('-')),
+                  SizedBox(width: 10),
+                  TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          foregroundColor: Colors.white,
+                          padding: EdgeInsets.all(16.0),
+                          textStyle: TextStyle(fontSize: 20)
+                      ),
+                      onPressed: () {},
+                      child: Text('*')),
+                  SizedBox(width: 10),
+                  TextButton(
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          foregroundColor: Colors.white,
+                          padding: EdgeInsets.all(16.0),
+                          textStyle: TextStyle(fontSize: 20)
+                      ),
+                      onPressed: () {},
+                      child: Text('/')),
+                ],
+              )
+            ],
+
+          ),
+        ),
       ),
     );
   }
 }
+
+
+// child: Padding(
+// padding: EdgeInsets.fromLTRB(10,30,10,0),
+// child: TextField(
+// onChanged: (newString){
+// print(newString);
+// username = newString;
+// },
+// keyboardType: TextInputType.text,
+// style: TextStyle(
+// fontSize: 25
+// ),
+// decoration: InputDecoration(
+// labelText: 'Username',
+// border: OutlineInputBorder(
+// borderRadius: BorderRadius.circular(10)
+// )
+// ),
+// ),
+// ),

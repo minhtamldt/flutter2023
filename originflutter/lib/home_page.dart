17df69689806eccb1473aa106f40041525ed39cf
phamtanphat <phatdroid94@gmail.com>
2023-02-25 21:29:27 +0700

    review code

--- originflutter/lib/home_page.dart
+++ originflutter/lib/home_page.dart
@@ -13,7 +13,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   late List<Restaurant> restaurants = Restaurant.Mock();
 
   @override
@@ -25,169 +24,139 @@ class _HomePageState extends State<HomePage> {
             constraints: BoxConstraints.expand(),
             child: ListView.separated(
               separatorBuilder: (BuildContext context, int index) =>
-              const Divider(
+                  const Divider(
                 height: 0, //remove padding cell
               ),
               itemCount: restaurants.length,
               itemBuilder: (context, index) {
-                final item = restaurants[index];
-                return InkWell(
-                  onTap: () =>
-                  {
-                    print(item.name)
-                  },
-                  child: Container(
-                    padding: EdgeInsets.only(
-                        left: 10, top: 5, right: 10, bottom: 5),
-                    height: 100,
-                    child: Row(
-                      children: [
-                        Container(
-                          width: 90,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
+                return renderItem(restaurants[index]);
+              },
+            )),
+      ),
+    );
+  }
+
+  Widget renderItem(Restaurant item) {
+    return InkWell(
+        onTap: () => {print(item.name)},
+        child: Padding(
+          padding: const EdgeInsets.all(5),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Flexible(
+                  flex: 2,
+                  child: AspectRatio(
+                    aspectRatio: 1 / 1,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Stack(
+                        children: [
+                          Image.network(
+                            fit: BoxFit.contain,
+                            item.overView,
+                          ),
+                          Visibility(
+                            visible: item.checkEndTimeOrder(),
                             child: Stack(
                               children: [
-                                Container(
-                                  constraints: BoxConstraints.expand(),
-                                  color: Colors.red,
-                                  child: FittedBox(
-                                      fit: BoxFit.fill,
-                                      child: Image.network(item.overView)),
-                                ),
-                                Visibility(
-                                  visible: item.checkEndTimeOrder(),
-                                  child: Stack(
-                                    children: [
-                                      Opacity(
-                                        opacity: 0.5,
-                                        child: Container(
-                                          color: Colors.black,
-                                        ),
-                                      ),
-                                      Container(
-                                        constraints: BoxConstraints.expand(),
-                                        child: Center(
-                                          child: Text(
-                                            "Hết hạn đặt món vào lúc: ${DateFormat(
-                                                'kk:mm').format(
-                                                item.endTimeOrder)}",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 11),
-                                            textAlign: TextAlign.center,),),
-                                      ),
-                                    ],
+                                Opacity(
+                                  opacity: 0.5,
+                                  child: Container(
+                                    color: Colors.black,
                                   ),
+                                ),
+                                Center(
+                                  child: Text(
+                                      "Hết hạn đặt món vào lúc: ${DateFormat('kk:mm').format(item.endTimeOrder)}",
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 11),
+                                      textAlign: TextAlign.center),
                                 )
                               ],
                             ),
-                          ),
-                        ),
-                        SizedBox(width: 10),
-                        Expanded(
-                            child: Column(
-                              children: [
-                                SizedBox(height: 5),
-                                Expanded(
-                                    child: Container(
-                                      child: Align(
-                                        alignment: Alignment.centerLeft,
-                                        child: FittedBox(
-                                          child: Text(item.name,
-                                            style: TextStyle(
-                                                color: Colors.black,
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 14),),),
-                                      ),
-                                    )),
-                                Expanded(
-                                    child: Row(
-                                      children: [
-                                        Expanded(
-                                          child: Container(
-                                            child: Align(
-                                              alignment: Alignment.centerLeft,
-                                              child: Text(
-                                                  item.address, maxLines: 1,
-                                                  overflow: TextOverflow
-                                                      .ellipsis,
-                                                  style: TextStyle(
-                                                      color: Color.fromARGB(
-                                                          255, 207, 207, 207),
-                                                      fontSize: 11)
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                        SizedBox(width: 3),
-                                        Text("> ${NumberFormat("###.#", "vi")
-                                            .format(item.distance)} km",
-                                            style: TextStyle(
-                                                color: Color.fromARGB(
-                                                    255, 207, 207, 207),
-                                                fontSize: 11))
-                                      ],
-                                    )),
-                                Expanded(
-                                    child: Container(
-                                      child: Align(
-                                        alignment: Alignment.centerLeft,
-                                        child: Text(
-                                            item.CategorysToString(),
-                                            style: TextStyle(
-                                                color: Colors.black,
-                                                fontSize: 11)
-                                        ),
-                                      ),
-                                    )),
-                                Expanded(
-                                    child: Row(
-                                      children: [
-                                        Container(
-                                          width: 20,
-                                          child: Image.asset(
-                                              "assets/images/icon_tag.png"),
-                                        ),
-                                        Expanded(child: Container(
-                                          child: RichText(
-                                            maxLines: 1,
-                                            text: TextSpan(
-                                                children: <TextSpan>[
-                                                  TextSpan(text: " ${item
-                                                      .openTimeMode.name} - ",
-                                                      style: TextStyle(
-                                                          color: item
-                                                              .getColorOpenTime(),
-                                                          fontSize: 11,
-                                                          fontWeight: FontWeight
-                                                              .bold)),
-                                                  TextSpan(
-                                                      text: item.preferential,
-                                                      style: TextStyle(
-                                                          color: Color.fromARGB(
-                                                              255, 243, 155,
-                                                              121),
-                                                          fontSize: 11,
-                                                          fontWeight: FontWeight
-                                                              .bold)),
-                                                ]
-                                            ),
-                                          ),
-                                        )),
-                                      ],
-                                    )),
-                                SizedBox(height: 5)
-                              ],
-                            )),
-                      ],
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              },
-            )
-        ),
-      ),
-    );
+                  )
+              ),
+              SizedBox(width: 10),
+              Flexible(
+                  flex: 8,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Flexible(
+                          child: Text(
+                            item.name,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14),
+                          )
+                      ),
+                      Flexible(
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Flexible(
+                                child: Text(
+                                    item.address,
+                                    style: TextStyle(
+                                        color: Color.fromARGB(255, 207, 207, 207),
+                                        fontSize: 11)
+                                ),
+                              ),
+                              SizedBox(width: 3),
+                              Text(
+                                  "> ${NumberFormat("###.#", "vi").format(item.distance)} km",
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 207, 207, 207),
+                                      fontSize: 11))
+                            ],
+                          )),
+                      Flexible(
+                          child: Text(item.CategorysToString(),
+                              style: TextStyle(color: Colors.black, fontSize: 11)
+                          )),
+                      Flexible(
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Container(
+                                width: 20,
+                                child: Image.asset("assets/images/icon_tag.png"),
+                              ),
+                              Flexible(
+                                  child: Container(
+                                    child: RichText(
+                                      maxLines: 1,
+                                      text: TextSpan(children: <TextSpan>[
+                                        TextSpan(
+                                            text: " ${item.openTimeMode.name} - ",
+                                            style: TextStyle(
+                                                color: item.getColorOpenTime(),
+                                                fontSize: 11,
+                                                fontWeight: FontWeight.bold)),
+                                        TextSpan(
+                                            text: item.preferential,
+                                            style: TextStyle(
+                                                color: Color.fromARGB(255, 243, 155, 121),
+                                                fontSize: 11,
+                                                fontWeight: FontWeight.bold)),
+                                      ]),
+                                    ),
+                                  )),
+                            ],
+                          )),
+                    ],
+                  )),
+            ],
+          ),
+        ));
   }
 }
